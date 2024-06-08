68827d2da5d4a5d981a1195163b29d95cbc65cc4
Gaston-Valentini <gastonvalentiniruiz@gmail.com>
2024-06-08 11:33:28 +0200

    Refactor: Converted into a component.

--- src/sections/HomeData/HomeData.jsx
+++ src/sections/HomeData/HomeData.jsx
@@ -1,33 +1,16 @@
 import style from "./HomeData.module.css";
 import { useTranslation } from "react-i18next";
-import flagSpain from "../../assets/flags/flag-spain.png";
-import flagEngland from "../../assets/flags/flag-england.png";
+import LanguageSelector from "../../components/LanguageSelector/LanguageSelector";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
 export default function HomeData() {
-    const [t, i18n] = useTranslation("global");
-    const [language, setLanguage] = useState("es");
-
-    const handleLanguage = (e) => {
-        const { value } = e.target;
-        setLanguage(value);
-        i18n.changeLanguage(value);
-    };
+    const { t } = useTranslation("global");
 
     return (
         <div className={style.container}>
             <p className={style.title}>SUNSET</p>
             <div className={style.language}>
-                <div className={style.languageFlag}>
-                    {language === "es" && <img src={flagSpain} />}
-                    {language === "en" && <img src={flagEngland} />}
-                </div>
-                <select className={style.languageSelector} onChange={(e) => handleLanguage(e)}>
-                    <option hidden>{t("home.homeData.language")}</option>
-                    {language === "es" && <option value="en">{t("home.homeData.languageEn")}</option>}
-                    {language === "en" && <option value="es">{t("home.homeData.languageEs")}</option>}
-                </select>
+                <LanguageSelector />
             </div>
             <p className={style.soon}>{t("home.homeData.soon")}</p>
             <Link to="/menu" className={style.link}>
