0897771eaee6e68aa3b6f71a9a55a82064d0cd9f
Gaston-Valentini <gastonvalentiniruiz@gmail.com>
2024-06-08 11:33:28 +0200

    Refactor: Language selector converted into a component.

---
+++ src/components/LanguageSelector/LanguageSelector.jsx
@@ -0,0 +1,30 @@
+import style from "./LanguageSelector.module.css";
+import { useTranslation } from "react-i18next";
+import flagSpain from "../../assets/flags/flag-spain.png";
+import flagEngland from "../../assets/flags/flag-england.png";
+import { useState } from "react";
+
+export default function LanguageSelector() {
+    const [t, i18n] = useTranslation("global");
+    const [language, setLanguage] = useState("es");
+
+    const handleLanguage = (e) => {
+        const { value } = e.target;
+        setLanguage(value);
+        i18n.changeLanguage(value);
+    };
+
+    return (
+        <div className={style.container}>
+            <div className={style.flag}>
+                {language === "es" && <img src={flagSpain} />}
+                {language === "en" && <img src={flagEngland} />}
+            </div>
+            <select className={style.selector} onChange={(e) => handleLanguage(e)}>
+                <option hidden>{t("home.homeData.language")}</option>
+                {language === "es" && <option value="en">{t("home.homeData.languageEn")}</option>}
+                {language === "en" && <option value="es">{t("home.homeData.languageEs")}</option>}
+            </select>
+        </div>
+    );
+}
